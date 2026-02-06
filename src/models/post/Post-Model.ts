8a74176a456d7dbb14081bd298e44cb4ae6ad45a
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2026-02-05 22:20:44 -0300

    configuração da entidade post e tsconfig

--- src/models/post/Post-Model.ts
+++ src/models/post/Post-Model.ts
@@ -1,12 +1,34 @@
-export type PostModel = {
-  id: string;
-  title: string;
-  slug: string;
-  excerpt: string;
-  content: string;
-  coverImageUrl: string;
-  published: boolean;
-  createdAt: string;
-  updatedAt: string;
-  author: string;
-};
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("posts")
+export class PostModel {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+  @Column()
+  title!: string;
+  @Column()
+  slug!: string;
+  @Column()
+  excerpt!: string;
+  @Column()
+  content!: string;
+  @Column()
+  coverImageUrl!: string;
+  @Column()
+  published!: boolean;
+  @CreateDateColumn()
+  createdAt!: string;
+  @UpdateDateColumn()
+  updatedAt!: string;
+  @Column()
+  author!: string;
+  @DeleteDateColumn()
+  deletedAt!: Date;
+}
