3f0fb5a757f9f2c3f464bb26680885c82757f3c8
joaovictor-jpg <joaovictormdasilva676@gmail.com>
2026-02-06 21:17:47 -0300

    migration

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -5,7 +5,11 @@ import { DataSource } from "typeorm";
 export const AppDataSource = new DataSource({
   type: "sqlite",
   database: "./src/db/database.sqlite",
-  synchronize: true,
+  migrations: [__dirname + "./src/db/migration/**/*{.js,.ts}"],
+  synchronize: false,
   logging: true,
   entities: [PostModel],
+  migrationsRun: false,
+  migrationsTableName: "migrations",
+  migrationsTransactionMode: "all",
 });
